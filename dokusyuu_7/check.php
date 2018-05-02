bd81ca9457e7cb419e7335d138e5dc838c773dd4
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-05-02 10:02:34 +0900

    独習php この章の理解度チェック.txt 修正 push

--- dokusyuu_7/check.php
+++ dokusyuu_7/check.php
@@ -5,17 +5,18 @@
 // 【2.回答】
 // DateTimeクラスをインスタンス化し$nowに代入。
 // 現在の時刻を取得したいので引数は指定しない
+const TIME_FORMAT = "Y年m月d日";
 $now = new DateTime('');
 // formatで()内に'Y年m月d日'と書式を指定しechoで$now（現在時刻）を出力
-echo $now -> format('Y年m月d日') . "<br>";
+echo $now -> format(TIME_FORMAT) . "<br>";
 // DateTimeクラスの引数に 'last day of' で月の末日を取得できるように指定し$m_last_dayに代入
 $m_last_day = new DateTime('last day of');
 // $nowと同じ書式で出力
-echo $m_last_day -> format('Y年m月d日') . "<br>";
+echo $m_last_day -> format(TIME_FORMAT) . "<br>";
 // $after_fourtydayにDateTimeクラスを代入
 $after_fourtyday = new DateTime('');
 // 現在時刻に new DateInterval で1ヶ月10日を指定し add で加算する
 $after_fourtyday -> add(new DateInterval('P1M10D'));
 // $nowと同じ書式で出力
-echo $after_fourtyday -> format('Y年m月d日') . "<br>";
+echo $after_fourtyday -> format(TIME_FORMAT) . "<br>";
 ?>
