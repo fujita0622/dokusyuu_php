9e7843b49a63420b0bda1ec1ce5b7c653e136899
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-05-08 17:23:35 +0900

    独習php 10章 この章の理解度チェック push

---
+++ dokusyuu_10/check.php
@@ -0,0 +1,49 @@
+<!--
+ 10章 この章の理解度チェック
+4. 練習問題10.5の2.の回答をインターフェイスを使って書き換えましょう。
+[練習問題10.5回答]
+  abstract class animal {
+    public abstract function outputVoice(): string{
+  }
+
+  class Cat extends Animal {
+    public final function outputVoice(): string {
+      return 'ニャァ';
+    }
+  }
+
+  class Dog extends Animal {
+    public final function outputVoice(): string {
+      return 'バゥ';
+  }
+
+  $cat = new Cat();
+  print $cat->outputVoice() . "</br>";
+  $dog = new Dog();
+  print $dog->outputVoice() . "</br>";
+?>
+-->
+
+<!-- [4.回答] -->
+<?php
+interface IFanimal {
+  function outputVoice(): string;
+}
+
+class Cat implements IFanimal {
+  function outputVoice(): string {
+    return 'ニャァ';
+  }
+}
+
+class Dog implements IFanimal {
+  function outputVoice(): string {
+    return 'バゥ';
+  }
+}
+
+$cat = new Cat();
+print $cat->outputVoice() . "</br>";
+$dog = new Dog();
+print $dog->outputVoice() . "</br>";
+?>
